9a26e7aa243d79fb8da35387d789d74567c4f80d
Binel Biju <binelbpe@gmail.com>
2024-10-27 20:26:13 +0530

    advanced search in user

--- dist/application/usecases/user/AdvancedJobSearchUseCase.js
+++ dist/application/usecases/user/AdvancedJobSearchUseCase.js
@@ -8,6 +8,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -17,41 +20,31 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.VerifyOtpUseCase = void 0;
-const OtpService_1 = require("../../../infrastructure/services/OtpService");
+exports.AdvancedJobSearchUseCase = void 0;
 const inversify_1 = require("inversify");
-let VerifyOtpUseCase = class VerifyOtpUseCase {
-    constructor(recruiterRepository, otpService) {
-        this.recruiterRepository = recruiterRepository;
-        this.otpService = otpService;
+const types_1 = __importDefault(require("../../../types"));
+let AdvancedJobSearchUseCase = class AdvancedJobSearchUseCase {
+    constructor(jobPostRepository) {
+        this.jobPostRepository = jobPostRepository;
     }
-    execute(email, otp) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const verificationResult = yield this.otpService.verifyOtp(email, otp);
-            if (typeof verificationResult === "string") {
-                throw new Error(verificationResult); // Handle error message from verifyOtp
+    execute(filters_1) {
+        return __awaiter(this, arguments, void 0, function* (filters, page = 1, limit = 10) {
+            try {
+                return yield this.jobPostRepository.advancedSearch(filters, page, limit);
             }
-            const isValid = verificationResult; // This will be true or false
-            if (isValid) {
-                const recruiter = yield this.recruiterRepository.findByEmail(email);
-                if (recruiter) {
-                    recruiter.isApproved = true;
-                    yield this.recruiterRepository.save(recruiter);
-                    return recruiter;
-                }
-                else {
-                    throw new Error("Recruiter not found");
-                }
-            }
-            else {
-                throw new Error("Invalid OTP");
+            catch (error) {
+                throw new Error("Failed to perform advanced job search");
             }
         });
     }
 };
-exports.VerifyOtpUseCase = VerifyOtpUseCase;
-exports.VerifyOtpUseCase = VerifyOtpUseCase = __decorate([
+exports.AdvancedJobSearchUseCase = AdvancedJobSearchUseCase;
+exports.AdvancedJobSearchUseCase = AdvancedJobSearchUseCase = __decorate([
     (0, inversify_1.injectable)(),
-    __metadata("design:paramtypes", [Object, OtpService_1.OtpService])
-], VerifyOtpUseCase);
+    __param(0, (0, inversify_1.inject)(types_1.default.IJobPostUserRepository)),
+    __metadata("design:paramtypes", [Object])
+], AdvancedJobSearchUseCase);
